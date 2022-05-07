50727dac3f5c25280db69099deae801052b36116
Mauro <40081963+progettazionemauro@users.noreply.github.com>
2022-05-07 13:46:34 +0000

    Completo Database e funzionante

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
 import sqlite3
 
 from flask_bootstrap import Bootstrap
@@ -34,6 +34,16 @@ def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+  
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+  
 
 @app.route('/databasevista/')
 def data_base():
@@ -42,6 +52,41 @@ def data_base():
     conn.close()
     return render_template('databasevista.html', posts=posts)
 
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+
+        elif not content:
+            flash('Content is required!')
+
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('data_base'))
+
+    return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete/', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('data_base'))
+  
 @app.route('/databasecrea/', methods=('GET', 'POST'))
 def create_messaggio():
     if request.method == 'POST':
